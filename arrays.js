3efef4fa7897202128fc9cc883538c944b1681fe
KlvnWr <kelvin.wartanabe@gmail.com>
2023-05-04 16:00:35 -0300

    commit - ready

--- arrays.js
+++ arrays.js
@@ -1,6 +1,21 @@
-const nome = 'Larissa Paixão Farache Silva'
+// const nome = 'Larissa Paixão Farache Silva'
 
-for (let i = 0; i < nome.length; i++) {
-   
-    console.log (nome[i])
+// for (let i = 0; i < nome.length; i++) {
+
+//     console.log (nome[i])
+// }
+
+const notas = [];
+
+notas.push(10);
+notas.push(6);
+// notas.push(4);
+
+let soma = 0;
+
+for (let i = 0; i < notas.length; i++) {
+    const nota = notas[i];
+    soma = soma + nota;
 }
+const media = soma / notas.length;
+console.log(media);
