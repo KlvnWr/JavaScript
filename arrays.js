781a824610f33140d6909b2605dfe7ed6254809d
KlvnWr <kelvin.wartanabe@gmail.com>
2023-05-05 18:31:28 -0300

    commit - amem que acabei

--- arrays.js
+++ arrays.js
@@ -20,9 +20,26 @@
 // const media = soma / notas.length;
 // console.log(media);
 
-const numero = 5
+// const numero = 5
 
-for (let i = 1; i < 11; i++) {
-    console.log(i * numero)
-}
+// for (let i = 1; i < 11; i++) {
+//     console.log(i * numero)
+// }
+
+// const numeros = [0,1,2,3,4,5,6,7,8,9,10];
+
+// for (let i = 0; i < numeros.length; i++) {
+//     const numero = numeros[i];
+//     if(numero % 2 === 0){
+//         console.log(numero)
+//     }
+// }
+
+const numerosPares = [];
+
+    for (let i = 0; i < 10; i = i + 2) {
+        numerosPares.push(i);
+    }
+
+    console.log(numerosPares);
     
